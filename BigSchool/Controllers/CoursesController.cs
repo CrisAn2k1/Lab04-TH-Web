d9e71bc338101612aa5493fc63aa6c8157d2dae3
unknown <duongquocan222@gmail.com>
2022-03-24 16:08:11 +0700

    BigSchool

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Courses/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Courses/Create
+        public ActionResult Create()
+        {
+            BigSchoolContext context = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.listCategory = context.Category.ToList();
+            return View(objCourse);
+        }
+
+        // POST: Courses/Create
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext context = new BigSchoolContext();
+
+            if (!ModelState.IsValid)
+            {
+                objCourse.listCategory = context.Category.ToList();
+                return View("Create", objCourse);
+            }
+
+
+            var dateTimeString = Request["txtDate"].ToString() + " " + Request["txtTime"].ToString();
+            objCourse.DateTime = DateTime.Parse(dateTimeString);
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+            context.Course.Add(objCourse);
+            context.SaveChanges();
+            return RedirectToAction("Create", "Courses");
+        }
+
+        // GET: Courses/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Courses/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Courses/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
